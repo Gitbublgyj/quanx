bcd25aa6e97b52e33b1b4c6f5b6e77bc680918f3
Gitbublgyj <96975409+Gitbublgyj@users.noreply.github.com>
2022-04-27 20:24:43 +0800

    Create slydapp.js

---
+++ slydapp.js
@@ -0,0 +1,4 @@
+hostname = *.beijzc.com
+[QX重写]
+v2/welfare url script-request-body http://script.frp.aiuyo.com:8680/scripts/slyd.js
+Verify/verifyLogin url script-request-body http://script.frp.aiuyo.com:8680/scripts/slyd.js
