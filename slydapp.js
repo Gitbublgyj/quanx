e4f15ad9b33d8cbf214c2a637700fb4b378a1158
Gitbublgyj <96975409+Gitbublgyj@users.noreply.github.com>
2022-07-29 21:35:06 +0800

    Delete slydapp.js

--- slydapp.js
+++
@@ -1,4 +0,0 @@
-hostname = *.beijzc.com
-[QX重写]
-v2/welfare url script-request-body http://script.frp.aiuyo.com:8680/scripts/slyd.js
-Verify/verifyLogin url script-request-body http://script.frp.aiuyo.com:8680/scripts/slyd.js
